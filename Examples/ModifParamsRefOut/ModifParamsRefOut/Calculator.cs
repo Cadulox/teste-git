66ac753693d7a703680c4d97f68a28f0b34e23b3
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-16 19:03:01 -0300

    Exemplos de Modificadores de Parâmetros

---
+++ Examples/ModifParamsRefOut/ModifParamsRefOut/Calculator.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ModifParamsRefOut {
+    class Calculator {
+
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Examples/ModifParamsRefOut/ModifParamsRefOut/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModifParamsRefOut {
+    class Program {
+        static void Main(string[] args) {
+
+            // Nota: ambos, REF e OUT, são considerados "code smells" (design ruim)
+            // e devem ser evitados.
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
