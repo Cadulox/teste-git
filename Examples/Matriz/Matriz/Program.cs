fd039e01d7920b0a9d8a67456ddba662657dbe66
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-24 10:33:09 -0300

    Exemplos utilizando Matriz

---
+++ Examples/Matriz/Matriz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matriz {
+    class Program {
+        static void Main(string[] args) {
+
+            double[,] mat = new double[2, 3];
+            // Instancia uma matriz de 2 linhas e 3 colunas
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+            // Mostra a quantidade de linhas
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
