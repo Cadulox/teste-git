62ed72f1545ec4058d02ca908f6e28879bea2933
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-24 12:00:06 -0300

    Exemplo utilizando Conjuntos

---
+++ Examples/Conjuntos/Conjuntos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Conjuntos {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+            //B.Remove(5);
+            // Remove um elemento do conjunto
+
+            //B.Add(4);
+            // Um conjunto não repete elemento
+
+            //A.ExceptWith(B);
+            // Remove de A o que é repetido em B
+
+            //A.UnionWith(B);
+            // Vai alterar A e unir com os elementos de B
+
+            A.IntersectWith(B);
+            // Vai fazer a intersecção de A e B
+
+            foreach (int x in A) {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Console.Write("Digite um valor inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (B.Contains(N)) {
+                Console.WriteLine(N + " pertence ao conjunto B");
+            }
+            else {
+                Console.WriteLine(N + " não pertence ao conjunto B");
+            }
+        }
+    }
+}
---
+++ Examples/ConjuntosExercise/ConjuntoExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConjuntoExercise {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.Write("O curso A possui quantos alunos? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso A:");
+
+            for (int i = 0; i < n; i++) {
+                int value = int.Parse(Console.ReadLine());
+                A.Add(value);
+            }
+
+            Console.Write("O curso B possui quantos alunos? ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso B:");
+
+            for (int i = 0; i < n; i++) {
+                int value = int.Parse(Console.ReadLine());
+                A.Add(value);
+            }
+
+            Console.Write("O curso C possui quantos alunos? ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso C:");
+
+            for (int i = 0; i < n; i++) {
+                int value = int.Parse(Console.ReadLine());
+                A.Add(value);
+            }
+
+            HashSet<int> Total = new HashSet<int>();
+            Total.UnionWith(A);
+            Total.UnionWith(B);
+            Total.UnionWith(C);
+            
+            Console.WriteLine();
+            Console.WriteLine("Total de alunos: " + Total.Count);
+        }
+    }
+}
