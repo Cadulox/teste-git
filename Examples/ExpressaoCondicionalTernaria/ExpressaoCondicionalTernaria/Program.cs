6c1d2e3888d676d9245ef43f468d0fe6d42d9bff
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-24 20:53:20 -0300

    Exemplo de Condicional Ternária

---
+++ Examples/ExpressaoCondicionalTernaria/ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressaoCondicionalTernaria {
+    class Program {
+        static void Main(string[] args) {
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            // Variável     = (condição)  ? se verdadeiro  :  se falso
+            
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
