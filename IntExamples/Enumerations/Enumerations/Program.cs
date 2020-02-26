a53b4898bc5cf4c09a88834d009d593c03996254
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-26 14:09:14 -0300

    Exemplo utilizando Enumerations

---
+++ IntExamples/Enumerations/Enumerations/Program.cs
@@ -0,0 +1,29 @@
+﻿using Enumerations.Entities;
+using Enumerations.Entities.Enums;
+using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
