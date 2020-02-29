558d368e9fbf69a692267d707196dedd5f880b0d
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-29 17:32:02 -0300

    Proposed exercise of inheritance and polymorphism

---
+++ IntExamples/InheritanceProposedExercise/InheritanceProposedExercise/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using InheritanceProposedExercise.Entities;
+
+namespace InheritanceProposedExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> listProd = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'c')
+                {
+                    listProd.Add(new Product(name, price));
+                }
+                else if (ch == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    listProd.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    listProd.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in listProd)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
