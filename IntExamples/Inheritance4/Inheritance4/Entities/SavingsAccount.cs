f2c5ad598afebe8b75c75fc4474764f534eb8bb2
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-29 11:33:33 -0300

    Example using class and sealed methods

---
+++ IntExamples/Inheritance4/Inheritance4/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace Inheritance4.Entities
+{
+    sealed class SavingsAccount : Account
+        // A classe selada evita que seja herdada em outra subclasse
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount)
+        // O método selado evita que não seja sobreposto em outra subclasse
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
