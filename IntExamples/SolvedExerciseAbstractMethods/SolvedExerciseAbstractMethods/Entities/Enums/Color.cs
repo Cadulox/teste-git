cca179afe5c211bea2d3bfee4e941b792ee96c27
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-01 22:47:41 -0300

    Solved exercise using abstract methods

---
+++ IntExamples/SolvedExerciseAbstractMethods/SolvedExerciseAbstractMethods/Entities/Enums/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace SolvedExerciseAbstractMethods.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ IntExamples/SolvedExerciseAbstractMethods/SolvedExerciseAbstractMethods/Entities/Shape.cs
@@ -0,0 +1,16 @@
+﻿using SolvedExerciseAbstractMethods.Entities.Enums;
+
+namespace SolvedExerciseAbstractMethods.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
