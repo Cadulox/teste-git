e079453314307acbe9c44cbf8687be507de790f3
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-07 22:33:57 -0300

    Example using Delegates

---
+++ AdvancedExamples/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DelegateExample.Services;
+
+namespace DelegateExample
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // tem o mesmo resultado da linha de cima, poré é mais verboso
+
+            // double result = op(a, b);
+            double result = op.Invoke(a, b); // tem o mesmo resultado da linha de cima
+            Console.WriteLine(result);
+        }
+    }
+}
