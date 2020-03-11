291a1da37b6ed988c03098bc12b05acd29e5e79d
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-10 21:51:36 -0300

    Example 'a' using Compare

---
+++ AdvancedExamples/ComparisonExample/ComparisonExample/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ComparisonExample.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //public int CompareTo(Product other)
+        //{
+        //    return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        //}
+
+        public int CompareTo(Product other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
