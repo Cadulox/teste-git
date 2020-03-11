646c272ccf110ef37dd7c4799838d5b3a99280ae
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-10 22:02:33 -0300

    Example 'b' using Comparison

---
+++ AdvancedExamples/ExampleComparisonB/ExampleComparisonB/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using ComparisonExampleB.Entities;
+
+namespace ComparisonExampleB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            //list.Sort(CompareProducts);
+
+            Comparison<Product> comp = CompareProducts;
+            list.Sort(comp);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
