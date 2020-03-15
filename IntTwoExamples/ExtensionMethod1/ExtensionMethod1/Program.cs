c9a0865c82e56cd7c315c6a6317fd15078a0e3e7
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-15 15:37:59 -0300

    Example 1 using Extension Methods

---
+++ IntTwoExamples/ExtensionMethod1/ExtensionMethod1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtensionMethod1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            DateTime dt = new DateTime(2020, 03, 15, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+    }
+}
