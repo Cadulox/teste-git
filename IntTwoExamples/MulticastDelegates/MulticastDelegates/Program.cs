1d34ded4ddbed71f50dcbbabda9ba72218a5ed87
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-15 16:52:55 -0300

    Example using Multicast Delegates

---
+++ IntTwoExamples/MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using MulticastDelegates.Services;
+
+namespace MulticastDelegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+            op(a, b); // também funciona igual ao Invoke
+        }
+    }
+}
