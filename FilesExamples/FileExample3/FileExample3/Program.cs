6d0b3cfa53c307f5119ec6dc96f578f6a67e12a4
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-16 21:28:16 -0300

    Simplified example using Using Block

--- FilesExamples/FileExample3/FileExample3/Program.cs
+++ FilesExamples/FileExample3/FileExample3/Program.cs
@@ -12,15 +12,12 @@ namespace FileExample3
 
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    using (StreamReader sr = new StreamReader(fs))
+                    while (!sr.EndOfStream)
                     {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
                     }
                 }
             }
