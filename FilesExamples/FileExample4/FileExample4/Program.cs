d45ce910d679429c17a4cf3638c12a316abd37ea
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-17 19:57:15 -0300

    Example using StreamWriter

---
+++ FilesExamples/FileExample4/FileExample4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcepath = @"c:\temp\teste.txt";
+            string targetpath = @"c:\temp\teste2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
