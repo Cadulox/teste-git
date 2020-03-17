f2b0897fcc95af8de5331bbfffe4c2c993fbbe65
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-16 21:26:46 -0300

    Example using Using Block

---
+++ FilesExamples/FileExample3/FileExample3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"c:\temp\teste.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
