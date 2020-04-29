02df2b6c91f0a89974f23731377b640781dcf309
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-29 14:41:58 -0300

    Improved entities

---
+++ Samples/PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class SudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Carlos", "Eduardo", "12345678912", "carlos@contato.com");
+            student.AddSubscription(subscription);
+        }
+    }
+
+}
